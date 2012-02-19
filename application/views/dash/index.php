23ff5926843e2bff150ee7d90857470c4d8b8645
Priyadarshi Lahiri <me@priyolahiri.co.cc>
2012-02-19 16:51:28 +0530

    template fix

--- application/views/dash/index.php
+++ application/views/dash/index.php
@@ -55,7 +55,7 @@
 				<div class="col_12" id="content_area">
 					<h3>Dashboard</h3>
 					<?php
-						$imgurl = ($socialauth->facebook_profile->photoURL != "NA") ? $socialauth->twitter_profile->photoURL : $socialauth->facebook_profile->photoURL;
+						$imgurl = ($socialauth->facebook_profile->photoURL == "NA") ? $socialauth->twitter_profile->photoURL : $socialauth->facebook_profile->photoURL;
 						$name = !$socialauth->twitter_profile->lastName ? $socialauth->facebook_profile->firstName.' '.$socialauth->facebook_profile->lastName : $socialauth->twiiter_profile->firstName
 					?>
 					<img src="<?php echo($imgurl) ?>">Hi, <?php echo($name) ?>. Welcome to your dashboard. Use the menu on the top to access your options. 
