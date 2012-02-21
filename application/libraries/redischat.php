6221d8ca2f632152dc0c06de804e655e3cf1a915
Priyadarshi Lahiri <me@priyolahiri.co.cc>
2012-02-21 07:47:59 +0530

    fix

--- application/libraries/redischat.php
+++ application/libraries/redischat.php
@@ -33,7 +33,7 @@ Class Redischat {
 	public function approve($id) {
 		$transport = json_decode($this->chatset[$id-1]);
 		$timenow = date('d/m/Y H:i', time());
-		$newtransport = json_encode(array('timenow' => $timenow, 'msg' => $transport['msg']));
+		$newtransport = json_encode(array('timenow' => $timenow, 'msg' => $transport->msg));
 		$this->chatset[] = $newtransport;
 		$this->modchatset->remove(json_encode($transport));
 		return json_encode(array("msgsuccess" => "approved!"));
