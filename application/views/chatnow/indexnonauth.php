bbc8ede5eb936cc2a36e549437e5ee1860224806
Priyadarshi Lahiri <me@priyolahiri.co.cc>
2012-02-25 19:16:37 +0530

    FIX

--- application/views/chatnow/indexnonauth.php
+++ application/views/chatnow/indexnonauth.php
@@ -125,9 +125,8 @@
 								var oldobj = jQuery.parseJSON(oldmsg);
 								var chattime = oldobj.timenow;
 								var chatmsg = oldobj.msg;
-								var output = '<li class="chat_element">At '+chattime+':</li>';
-								var output2 = '<li class="chat_element">'+chatmsg+'<hr/></li>';
-								$('#chat_main_inner').append(output+output2);
+								var output = '<li class="chat_element">'+chatmsg+'<hr/></li>';
+								$('#chat_main_inner').append(output);
 								var elem = document.getElementById('chat_main_inner');
   								elem.scrollTop = elem.scrollHeight;
 							});
