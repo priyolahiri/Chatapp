0cdd94cb14218b75acae7711b28bf3c792d97d6a
Priyadarshi Lahiri <me@priyolahiri.co.cc>
2012-05-01 14:52:04 +0530

    fix

--- application/views/chatnow/indexnonauth.php
+++ application/views/chatnow/indexnonauth.php
@@ -10,6 +10,7 @@
 		<!-- <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"></script> -->
 		<script type="text/javascript" src="/js/underscore.min.js"></script>
 		<script type="text/javascript" src="/js/ajaxfileupload.js"></script>
+		<script type="text/javascript" src="/js/jquery.fancybox-1.3.4.pack.js"></script>
 		<script type="text/javascript" src="/js/jquery.easing-1.3.pack.js"></script>
 		<!-- <script type="text/javascript" src="/js/jquery.mousewheel-3.0.4.pack.js"></script> -->
 		<script type="text/javascript" src="/js/jquery.prettyLoader.js"></script>
